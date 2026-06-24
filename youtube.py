aafe04246499eed338b246d7bfa2fc804d00d45f
mono3020 <sn121254@seifunankai.net>
2026-06-25 07:59:32 +0900

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -3,64 +3,22 @@ import yt_dlp
 
 def get_video_info(url):
 
-
     options = {
 
         "quiet": True,
 
         "format":
-        "best[ext=mp4]/best",
-
-        "nocheckcertificate": True,
-
-        "http_headers": {
-
-            "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-
-        },
-
-        "extractor_args": {
-
-            "youtube": {
-
-                "player_client": [
-                    "web"
-                ]
-
-            }
-
-        }
+        "best[ext=mp4]/best"
 
     }
 
 
-
     with yt_dlp.YoutubeDL(options) as ydl:
-
 
         info = ydl.extract_info(
             url,
             download=False
         )
-
-
-    size = info.get(
-        "filesize"
-    )
-
-
-    if size:
-
-        mb = round(
-            size / 1024 / 1024,
-            2
-        )
-
-    else:
-
-        mb = "不明"
-
 
 
     return {
@@ -69,9 +27,6 @@ def get_video_info(url):
         info.get("title"),
 
         "download_url":
-        info.get("url"),
-
-        "size":
-        mb
+        info.get("url")
 
     }
